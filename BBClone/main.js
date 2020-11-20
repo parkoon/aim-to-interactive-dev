9c24c2dc3f18a6ebbcebb953e6c69e4143b043a6
parkoon <devparkoon@gmail.com>
2020-11-21 00:16:37 +0900

    feat: intersection observer 사용해서 최적화하기
    for문을 모두 도는것이 아닌, intersection observer 를 활용해서 앞 뒤에 index를 찾고 for문을!

--- BBClone/main.js
+++ BBClone/main.js
@@ -3,15 +3,19 @@
   const graphicElems = document.querySelectorAll(".graphic-item");
 
   let currentItem = graphicElems[0]; // 현재 활성화된 그래픽
+  let ioIndex;
+
+  // 관찰하고 있는 객체가 생기거나 사라질 떄 콜백 함수가 실행된다.
+  const io = new IntersectionObserver((entries, observer) => {
+    ioIndex = entries[0].target.dataset.index * 1;
+  });
 
   // HTML 에 직접 입력해줘도 되지만 스크립트로 처리
-  stepElems.forEach((el, i) => {
-    el.dataset.index = i;
-  });
-  graphicElems.forEach((el, i) => {
-    // el.setAttribute("data-index", i);
-    el.dataset.index = i;
-  });
+  for (let i = 0; i < stepElems.length; i++) {
+    io.observe(stepElems[i]);
+    stepElems[i].dataset.index = i;
+    graphicElems[i].dataset.index = i;
+  }
 
   function activate() {
     currentItem.classList.add("visible");
@@ -26,8 +30,11 @@
     let boundingRect;
 
     // 루프를 다 돌 필요가 있을까? 근처에 있는 애들의 위치만 체크하면 될 것 같은데 --> intersection observer 로 눈에 보이는 애들을 확인 할 수 있다.
-    for (let i = 0; i < stepElems.length; i++) {
+    // for (let i = 0; i < stepElems.length; i++) {
+    for (let i = ioIndex - 1; i < ioIndex + 2; i++) {
       step = stepElems[i];
+
+      if (!step) continue;
       boundingRect = step.getBoundingClientRect();
 
       // window 10% ~ 80% 사이에 들어온다면
