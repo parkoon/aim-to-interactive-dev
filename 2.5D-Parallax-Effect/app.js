33116fccb2bb5249d25214d86d84eaae3514a9c8
parkoon <devparkoon@gmail.com>
2020-11-25 23:25:39 +0900

    feat: add script

---
+++ 2.5D-Parallax-Effect/app.js
@@ -0,0 +1,11 @@
+function parallax(element, distance, speed) {
+  const item = document.querySelector(element);
+
+  item.style.transform = `translateY(${distance * speed}px)`;
+}
+
+window.addEventListener("scroll", () => {
+  parallax("header", window.scrollY, 1);
+  parallax(".small-rose", window.scrollY, 1.5);
+  parallax(".big-rose", window.scrollY, 0.5);
+});
