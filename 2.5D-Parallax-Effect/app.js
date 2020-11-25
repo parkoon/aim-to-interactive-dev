738e7e039df9d5576fc04d159b1822acbeec09c2
parkoon <devparkoon@gmail.com>
2020-11-25 23:40:52 +0900

    fix: 계속 스크롤 되는 부분 막기위해 sticky로 변경

--- 2.5D-Parallax-Effect/app.js
+++ 2.5D-Parallax-Effect/app.js
@@ -1,11 +1,12 @@
-function parallax(element, distance, speed) {
+function parallax(element, distance, speed, zAxis) {
   const item = document.querySelector(element);
 
-  item.style.transform = `translateY(${distance * speed}px)`;
+  item.style.transform = zAxis
+    ? `translateY(${distance * speed}px) translateZ(${distance * speed}px)`
+    : `translateY(${distance * speed}px)`;
 }
 
 window.addEventListener("scroll", () => {
-  parallax("header", window.scrollY, 1);
-  parallax(".small-rose", window.scrollY, 1.5);
-  parallax(".big-rose", window.scrollY, 0.5);
+  parallax(".small-rose", window.scrollY, 1);
+  parallax(".big-rose", window.scrollY, 0.5, true);
 });
