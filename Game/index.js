474e4cf450a42af1311d1d61051cf74057142ecd
parkoon <devparkoon@gmail.com>
2020-11-27 23:21:31 +0900

    feat: add player and projectitle

---
+++ Game/index.js
@@ -0,0 +1,65 @@
+const canvas = document.querySelector("canvas");
+
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+}
+
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const player = new Player(x, y, 30, "blue");
+player.draw();
+
+const projectile = new Projectile(x, y, 10, "red", {
+  x: 1,
+  y: 1,
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  projectile.draw();
+  projectile.update();
+}
+
+window.addEventListener("click", (e) => {});
+
+animate();
