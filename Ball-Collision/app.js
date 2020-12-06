413315c80d65080b90ee530270ab23279f6e6dfd
parkoon <devparkoon@gmail.com>
2020-12-06 09:58:09 +0900

    feat: step 2. requestAnimationFrame
    window.setTimeout 와 다른점은 브라우저가 스스로 실행 시기을 지정하고
    
    window.setInterval 와 다른점은 반복해서 호출하지 않는다는 점이다.
    
    스스로 반복해서 호출하지 않기 때문에 재귀적으로 다시 호출해줘야 한다.

--- Ball-Collision/app.js
+++ Ball-Collision/app.js
@@ -8,6 +8,8 @@ class App {
     // 가변적인 스크린 사이즈 대응을 위해 리사이즈 이벤트를 처리해주는 것이 좋다.
     window.addEventListener("resize", this.resize.bind(this), false);
     this.resize();
+
+    window.requestAnimationFrame(this.animate.bind(this));
   }
 
   resize() {
@@ -18,6 +20,10 @@ class App {
     this.canvas.height = this.stageWidth * 2;
 
     this.ctx.scale(2, 2);
+  }
+
+  animate(t) {
+    window.requestAnimationFrame(this.animate.bind(this));
   }
 }
 
